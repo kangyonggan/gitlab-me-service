51ff331bbc5adc65e1053bacfb250108395655b4
开一个 <java@kangyonggan.com>
2020-06-14 22:22:52 +0800

    #7 profile

--- src/main/java/com/kangyonggan/gitlab/controller/BaseController.java
+++ src/main/java/com/kangyonggan/gitlab/controller/BaseController.java
@@ -3,6 +3,7 @@ package com.kangyonggan.gitlab.controller;
 import com.kangyonggan.gitlab.constants.Resp;
 import com.kangyonggan.gitlab.dto.Response;
 import com.kangyonggan.gitlab.interceptor.ParamsInterceptor;
+import com.kangyonggan.gitlab.model.User;
 import com.kangyonggan.gitlab.util.IpUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -52,4 +53,23 @@ public class BaseController {
     protected String getIpAddress() {
         return IpUtil.getIpAddress(ParamsInterceptor.getRequest());
     }
+
+    /**
+     * 获取当前用户
+     *
+     * @return
+     */
+    protected User currentUser() {
+        return ParamsInterceptor.getUser();
+    }
+
+    /**
+     * 获取当前用户ID
+     *
+     * @return
+     */
+    protected Long currentUserId() {
+        return ParamsInterceptor.getUser().getId();
+    }
+
 }
---
+++ src/main/java/com/kangyonggan/gitlab/controller/profile/ProfileController.java
@@ -0,0 +1,67 @@
+package com.kangyonggan.gitlab.controller.profile;
+
+import com.kangyonggan.gitlab.annotation.PermissionLogin;
+import com.kangyonggan.gitlab.controller.BaseController;
+import com.kangyonggan.gitlab.dto.Response;
+import com.kangyonggan.gitlab.model.User;
+import com.kangyonggan.gitlab.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author kyg
+ */
+@RestController
+@RequestMapping("profile")
+public class ProfileController extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 更新个人信息
+     *
+     * @param avatar
+     * @param fullName
+     * @param email
+     * @return
+     */
+    @PutMapping
+    @PermissionLogin
+    public Response update(@RequestParam String avatar, @RequestParam String fullName, @RequestParam String email) {
+        Response response = successResponse();
+
+        User user = new User();
+        user.setId(currentUserId());
+        user.setFullName(fullName);
+        user.setEmail(email);
+        user.setAvatar(avatar);
+
+        userService.updateUser(user);
+        user = userService.getUser(user.getId());
+
+        response.put("user", user);
+        return response;
+    }
+
+    /**
+     * 删除头像
+     *
+     * @return
+     */
+    @DeleteMapping("avatar")
+    @PermissionLogin
+    public Response removeAvatar() {
+        Response response = successResponse();
+
+        User user = new User();
+        user.setId(currentUserId());
+        user.setAvatar("");
+
+        userService.updateUser(user);
+        user = userService.getUser(user.getId());
+
+        response.put("user", user);
+        return response;
+    }
+}
